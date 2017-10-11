05830a330db36d40ed6150c1deb4d8ca0a42505f
Darcy Hu <hot123tea123@gmail.com>
2017-10-11 23:22:29 +0800

    补全了 im::on_action_Open_triggered() 函数的实现
    ------
    
    具体地说，就是打开文件，判定文件有效，然后调用 im::clearImage()
        函数清除之前的图像，然后显示新的图像．
    有一点错误，就是之前没有初始化 inScene 和 outScene
    对象，也没有将他们和界面中对应的控件绑定起来．现在只需在 im
    的构造函数中补充一下就好了．

--- im.cpp
+++ im.cpp
@@ -6,6 +6,13 @@ im::im(QWidget *parent) :
     ui(new Ui::im)
 {
     ui->setupUi(this);
+
+    // 初始化 QGraphicsScene，将 UI 中的控件与 im 类中对应的对象联系起来
+    inScene = new QGraphicsScene;
+    outScene = new QGraphicsScene;
+    ui->graphicsView_in->setScene(inScene);
+    ui->graphicsView_out->setScene(outScene);
+
 }
 
 im::~im()
@@ -27,5 +34,26 @@ void im::on_action_Quit_triggered()
 
 void im::on_action_Open_triggered()
 {
+    QString imagePath = QFileDialog::getOpenFileName(this, tr("Open file"), QDir::currentPath(), tr("All Images (*.bmp *.jpg *.jpeg *.png *.tif *.tiff);;"));
 
+    if (!imagePath.isEmpty()) {
+        QFile file(imagePath);
+
+        if(!file.open(QIODevice::ReadOnly)) {
+            QMessageBox::critical(this, tr("Error"), tr("Unable to read image!"));
+            return;
+        }
+
+        // 清除之前显示的图片，然后再显示
+        cleanImage();
+
+        // 显示图片
+        QPixmap inPixmap(imagePath);
+        inPixmapItem = inScene->addPixmap(inPixmap);
+        outScene->setSceneRect(QRectF(inPixmap.rect()));
+
+        QPixmap outPixmap(imagePath);
+        outPixmapItem = outScene->addPixmap(outPixmap);
+        outScene->setSceneRect(QRectF(outPixmap.rect()));
+    }
 }
